a3a80fb370971d4cb0bf1764eddc400190a9620e
nishant <n.rajadhyaksha@somaiya.edu>
2021-07-26 20:02:15 +0530

    Authentication/Sign_up

--- VideoSharing/urls.py
+++ VideoSharing/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('',HomePageView.as_view(),name='home'),
 ]
