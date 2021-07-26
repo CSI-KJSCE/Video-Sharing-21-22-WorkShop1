455c0d967c4cb9b4e78adea3e1dd6fbf72e5940f
Saurav <saurav.vs@somaiya.edu>
2021-07-26 13:29:03 +0530

    backend almost done

---
+++ website/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.db.models.fields import CharField, TextField
+
+# Create your models here.
+
+
+class User(models.Model):
+
+    firstname = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+
+
+    username = models.CharField(max_length=30, unique=True, primary_key=True)
+    emailID = models.EmailField(unique=True)
+    pfp = models.ImageField()
+
+
+
+    def __str__(self):
+        return self.username
+    
+
+class Video(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=100)
+    description = TextField(max_length=1000)
+
+
+
+    def __str__(self):
+        return self.user.username + ' : ' + self.title
+    
