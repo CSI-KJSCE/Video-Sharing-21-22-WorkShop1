e60ef1f7f450d0bb3f173ecbf49d198e0393b9ba
KrutikaBhatt <krutikabhatt222@gmail.com>
2021-07-28 16:36:14 +0530

    Started with homepage

--- website/views.py
+++ website/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+
+def homepage(request):
+    return render(request,'homepage.html')
