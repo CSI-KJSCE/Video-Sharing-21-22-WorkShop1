6da99e5c7aaf48bcce25ba94e64a8b361b35a9e3
KrutikaBhatt <krutikabhatt222@gmail.com>
2021-08-03 12:07:07 +0530

    Comment section added

--- website/models.py
+++ website/models.py
@@ -8,8 +8,6 @@ class User(models.Model):
 
     firstname = models.CharField(max_length=30)
     lastname = models.CharField(max_length=30)
-
-
     username = models.CharField(max_length=30, unique=True, primary_key=True)
     emailID = models.EmailField(unique=True)
     pfp = models.ImageField()
@@ -25,9 +23,12 @@ class Video(models.Model):
 
     title = models.CharField(max_length=100)
     description = TextField(max_length=1000)
-
-
-
     def __str__(self):
         return self.user.username + ' : ' + self.title
     
+class Comment(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    text = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.user.username
