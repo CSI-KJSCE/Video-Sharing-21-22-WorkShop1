d91acbacffb2a5b3ee2c3865de1a141ec131779a
Galahad2819 <akhilnagar19@gmail.com>
2021-08-09 23:19:31 +0530

    Like Disklike button

--- website/models.py
+++ website/models.py
@@ -8,35 +8,7 @@ from django.contrib.auth.models import User
 def __str__(self):
     return self.user.username
 
-'''
-class User(models.Model):
 
-    firstname = models.CharField(max_length=30)
-    lastname = models.CharField(max_length=30)
-    username = models.CharField(max_length=30, unique=True, primary_key=True)
-    emailID = models.EmailField(unique=True)
-    pfp = models.ImageField()
-
-
-
-    def __str__(self):
-        return self.username
-'''
-'''
-class Video(models.Model):
-    #user = models.ForeignKey(User, on_delete=models.CASCADE,default="")
-    title = models.CharField(max_length=100,default="")
-    description = TextField(max_length=1000,default="")
-    likes = models.IntegerField(default=0)
-    dislikes = models.IntegerField(default=0)
-    thumbnail = models.ImageField(default = None)
-    video = models.FileField(upload_to='videos_uploaded',validators=[FileExtensionValidator(allowed_extensions=['MOV','avi','mp4','webm','mkv'])], default=None)
-
-    #thumbnail= models.ImageField()
-
-    def __str__(self):
-        return self.title
-'''
 
 class NewVideo(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE,default="")
--- website/views.py
+++ website/views.py
@@ -1,76 +1,45 @@
 from django.shortcuts import render
 from django.contrib import messages
-<<<<<<< HEAD
+
 from .models import User, Comment, NewVideo
 from datetime import date
-=======
-from .models import User, Video, Comment
 from django.db.models import Q
->>>>>>> origin/nishant
-# Create your views here.
-
-
 from django.views.generic import TemplateView,ListView
+
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
 
 def homepage(request):
-    #username=User.objects.filter(name=User.username)
-    #videos=Video.objects.all()
+
     return render(request,'homepage.html',{})
-#"user":user, "video":videos
+
 
 def video(request,pk):
     video = NewVideo.objects.get(pk=pk)
-
-    #comments=Comments.objects.all()
-    #if request.method=="POST":
     return render(request,'videoView.html',{'video':video})
-        #Make the thumbs up and down icon a button
-        #likes++ dislikes++
-        #comment=request.POST["comment"]
-        # add videoid=
-        #commentobj=Comment(user=User.username,comment=comment) #add which video he had selected.
 
-    #{"comments":comments}
 
 
 def upload(request):
     if request.method == "POST":
         title = request.POST['title']
         desc = request.POST['desc']
-
         thumbnail =  request.FILES['thumbnail']
         video =  request.FILES['video']
-
-<<<<<<< HEAD
-
         videoobj= NewVideo(user=request.user,title=title,description=desc, date=date.today(),thumbnail=thumbnail,video=video )
         videoobj.save()
-
         #print('\n\n\n' + request.user + '\n\n')
-
     return render(request,'upload.html',{})
-=======
-        mid = Video()
-        mid.title = title
-        mid.description = desc
-        mid.thumbnail = thumbnail
 
-
-        print('\n\n\n' + request.user + '\n\n')
-
-    return render(request,'upload.html',{})
 
 class SearchResultsView(ListView):
-    model = Video
+    model = NewVideo
     template_name = 'search_results.html'
 
 
     def get_queryset(self):
         query = self.request.GET.get('q')
         return Video.objects.filter(
-            Q(title__icontains=query) 
+            Q(title__icontains=query)
         )
->>>>>>> origin/nishant
