4e8dbab4c2dadad1e72174ec8224cd9463892738
KrutikaBhatt <krutikabhatt222@gmail.com>
2021-08-10 01:24:49 +0530

    Final merge conflicts fixed

--- website/views.py
+++ website/views.py
@@ -2,11 +2,8 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 
 from .models import User, Comment, NewVideo
-<<<<<<< HEAD
 from django.http import HttpResponseRedirect
-=======
 from datetime import date
->>>>>>> 80623d2e01c1ad403eda62d8c9dc1067339b0d8f
 from django.db.models import Q
 from django.views.generic import TemplateView,ListView
 
@@ -22,7 +19,6 @@ def homepage(request):
 
 def video(request,pk):
     video = NewVideo.objects.get(pk=pk)
-<<<<<<< HEAD
     print(video)
     comments=Comment.objects.filter(video = video)
     count = Comment.objects.filter(video = video).count()
@@ -46,14 +42,10 @@ def video(request,pk):
         #comment=request.POST["comment"]
         # add videoid=
         #commentobj=Comment(user=User.username,comment=comment) #add which video he had selected.
-=======
-    return render(request,'videoView.html',{'video':video})
->>>>>>> 80623d2e01c1ad403eda62d8c9dc1067339b0d8f
 
 
 
 def upload(request):
-<<<<<<< HEAD
     if not request.user.is_authenticated:
         return HttpResponseRedirect('/accounts/login/')
     else:
@@ -66,17 +58,6 @@ def upload(request):
             videoobj= NewVideo(user=request.user,title=title,description=desc, date=date.today(),thumbnail=thumbnail,video=video )
             videoobj.save()
         return render(request,'upload.html',{})
-=======
-    if request.method == "POST":
-        title = request.POST['title']
-        desc = request.POST['desc']
-        thumbnail =  request.FILES['thumbnail']
-        video =  request.FILES['video']
-        videoobj= NewVideo(user=request.user,title=title,description=desc, date=date.today(),thumbnail=thumbnail,video=video )
-        videoobj.save()
-        #print('\n\n\n' + request.user + '\n\n')
-    return render(request,'upload.html',{})
->>>>>>> 80623d2e01c1ad403eda62d8c9dc1067339b0d8f
 
 
 class SearchResultsView(ListView):
@@ -86,6 +67,6 @@ class SearchResultsView(ListView):
 
     def get_queryset(self):
         query = self.request.GET.get('q')
-        return Video.objects.filter(
+        return NewVideo.objects.filter(
             Q(title__icontains=query)
         )
